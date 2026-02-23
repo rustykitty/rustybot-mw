4bfaaa3c1021acd5b3777d95436d90e06a7024b8
Rusty <rusty3925@gmail.com>
2026-02-22 22:14:44 -0800

    new bot file (read only, does not make edits) to track pages w/ tag and table

---
+++ task_3/tagged_with_episode_table.py
@@ -0,0 +1,45 @@
+import re
+import os
+
+import pywikibot
+import mwparserfromhell
+
+site = pywikibot.Site('en', 'wikipedia')
+
+list_page = pywikibot.Page(site, 'User:Alex 21/sandbox/No episode table')
+template_page = pywikibot.Page(site, "Template:Convert to Episode table")
+
+WRITE_TO_FILE = False
+
+TAG_STR = "{{Convert to Episode table}}\n"
+
+tagged_with_table = set()
+
+#for page in list_page.linkedPages(
+#    namespaces=[0], follow_redirects=True, content=True, total=None
+#    ):
+
+pages = list(template_page.getReferences(only_template_inclusion=True))
+
+for page in pages:
+
+    page: pywikibot.Page
+
+    templates = tuple(page.title(with_ns=False) for page in page.itertemplates())
+
+    if "Convert to Episode table" in templates and "Episode table" in templates:
+        print("added", page.title())
+        tagged_with_table.add(page.title())
+
+tagged_with_table = sorted(tagged_with_table)
+
+page_contents = f"""
+The following is a list of pages transclude both {{{{tl|Convert to episode table}}}} and {{{{tl|Episode table}}}}.
+""" + '\n'.join(f"*[[{page}]]" for page in tagged_with_table)
+
+if WRITE_TO_FILE:
+    open("tagged_with_table.txt", "w").write(page_contents)
+else:
+    output_page = pywikibot.Page(site, "User:RustyBot/Pages using Episode table tagged with Convert to Episode table")
+    output_page.text = page_contents
+    output_page.save("Updating page list", bot=True)
