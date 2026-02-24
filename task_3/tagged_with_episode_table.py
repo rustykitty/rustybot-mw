ae36a531c1a09e8a6f8f2d00e9c1e0fab0ff6a22
Rusty <rusty3925@gmail.com>
2026-02-23 17:15:31 -0800

    Edit text a bit

--- task_3/tagged_with_episode_table.py
+++ task_3/tagged_with_episode_table.py
@@ -33,9 +33,13 @@ for page in pages:
 
 tagged_with_table = sorted(tagged_with_table)
 
-page_contents = f"""
-The following is a list of pages transcluding both {{{{tl|Convert to Episode table}}}} and {{{{tl|Episode table}}}}.
-""" + '\n'.join(f"*[[{page}]]" for page in tagged_with_table)
+page_contents = \
+f"""The following is a list of pages transcluding both {{{{tl|Convert to Episode table}}}} and {{{{tl|Episode table}}}}.
+
+Last updated ~~~~~.
+
+{'\n'.join(f"*[[{page}]]" for page in tagged_with_table)}
+"""
 
 if WRITE_TO_FILE:
     open("tagged_with_table.txt", "w").write(page_contents)
